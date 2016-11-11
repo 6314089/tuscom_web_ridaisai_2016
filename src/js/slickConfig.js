f26c105d8e085ec83eee9982f599fdd7903e7056
6314089 <6314089@ed.tus.ac.jp>
2016-11-11 23:06:48 +0000

    slickの設定を分離した

---
+++ src/js/slickConfig.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('#slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    prevArrow: "#slider-left-arrow",
+    nextArrow: "#slider-right-arrow",
+  });
+});
