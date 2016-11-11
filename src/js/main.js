de56713c9a6b7c6c52d3e36040961621bcc9c41b
satorf <satorf.t@ed.tus.ac.jp>
2016-11-11 23:06:48 +0000

    slickの設定を分離した

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,4 @@
 require('../../node_modules/jquery-migrate/dist/jquery-migrate.min.js')
 require('slick-carousel');
 
-$(document).ready(function(){
-  $('#slider').slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    arrows: true,
-    prevArrow: "#slider-left-arrow",
-    nextArrow: "#slider-right-arrow",
-  });
-});
+import './slickConfig.js';
