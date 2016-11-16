6541cbcd0d807f1483e37c8178bd70f26b65da06
6314089 <6314089@ed.tus.ac.jp>
2016-11-16 23:03:06 +0000

    Barbaの読み込みを直す

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
+import Barba from 'barba.js';
 import slickOption from './slickOption';
 
 require('../../node_modules/jquery-migrate/dist/jquery-migrate.min');
 require('slick-carousel');
-require('barba.js');
 
 $(() => $('#slider').slick(slickOption));
 
-Barba.Pjax.start();
+$(() => Barba.Pjax.start());
