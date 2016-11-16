e8fb6bb62be3b55f7c081f9f7d52d73cc5398d66
satorf <satorf.t@ed.tus.ac.jp>
2016-11-16 22:45:14 +0000

    barba.jsを読み込む

--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,8 @@ import slickOption from './slickOption';
 
 require('../../node_modules/jquery-migrate/dist/jquery-migrate.min');
 require('slick-carousel');
+require('barba.js');
 
 $(() => $('#slider').slick(slickOption));
+
+Barba.Pjax.start();
