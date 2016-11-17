83cf1521d155f5dfa88e7db89af840b47d182a9d
6314089 <6314089@ed.tus.ac.jp>
2016-11-17 03:34:47 +0000

    barbaでslickを動くように

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,6 @@ import slickOption from './slickOption';
 require('../../node_modules/jquery-migrate/dist/jquery-migrate.min');
 require('slick-carousel');
 
-$(() => $('#slider').slick(slickOption));
 Barba.Dispatcher.on('newPageReady', () => $('#slider').slick(slickOption));
 
 $(() => Barba.Pjax.start());
