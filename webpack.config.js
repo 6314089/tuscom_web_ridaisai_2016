53fd03cd8ddae93ed6fbd2d97ff93eddc4eb8e1e
6314089 <6314089@ed.tus.ac.jp>
2016-11-17 17:33:33 +0000

    buildの設定を変えた

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   context: path.join(__dirname, '/src'),
   entry: ['babel-polyfill', './js/main.js'],
   output: {
-    path: path.join(__dirname, '/dist/ridaisai2016'),
+    path: path.join(__dirname, '/dist'),
     filename: 'bundle.js',
   },
   module: {
