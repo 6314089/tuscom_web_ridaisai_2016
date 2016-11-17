7ec11743805560d63db5dfd3c6cea166dc2c9f67
6314089 <6314089@ed.tus.ac.jp>
2016-11-17 17:56:01 +0000

    pugのcompileとファイルへの出力

--- compilePug.js
+++ compilePug.js
@@ -1,6 +1,9 @@
+const fs = require('fs');
+const path = require('path');
 const pug = require('pug');
 const data = require('./src/data');
 
+const dist = path.join(__dirname, '/dist');
 const option = {
   pretty: true,
 };
@@ -8,5 +11,20 @@ const option = {
 const top = pug.compileFile('./src/pug/top.pug', option);
 const single = pug.compileFile('./src/pug/single.pug', option);
 
-console.log(top(data.top));
-data.single.forEach(obj => console.log(single(obj)));
+fs.writeFile(
+  path.join(dist, 'top.html'),
+  top({}),
+  (err) => {
+    if (err) throw err;
+    console.log('top.html 完了');
+  });
+
+data.forEach((obj) => {
+  fs.writeFile(
+    path.join(dist, obj.fileName),
+    single(obj),
+    (err) => {
+      if (err) throw err;
+      console.log(`${obj.fileName} 完了`);
+    });
+});
