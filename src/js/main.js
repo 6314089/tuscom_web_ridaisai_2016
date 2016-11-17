e1f31f5cc811855090304ee46ef07644f191cf51
6314089 <6314089@ed.tus.ac.jp>
2016-11-17 09:46:17 +0000

    slickのエラーを出さないように

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,10 @@ import slickOption from './slickOption';
 require('../../node_modules/jquery-migrate/dist/jquery-migrate.min');
 require('slick-carousel');
 
-Barba.Dispatcher.on('newPageReady', () => $('#slider').slick(slickOption));
+Barba.Dispatcher.on('newPageReady', (currentStatus) => {
+  if (currentStatus.url.split('/').pop() === 'ridaisai2016.html') {
+    $('#slider').slick(slickOption);
+  }
+});
 
 $(() => Barba.Pjax.start());
