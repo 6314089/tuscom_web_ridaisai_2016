0100eba7f3ae70143ee966e865cff298846d6aaa
satorf <satorf.t@ed.tus.ac.jp>
2016-11-17 17:56:01 +0000

    pugのcompileとファイルへの出力

--- src/data.js
+++ src/data.js
@@ -1,8 +1,28 @@
-module.exports = {
-  top: {},
-  single: [
-    { name: 'Tanaka' },
-    { name: 'Nakata' },
-    { name: 'Sato' },
-  ],
-};
+module.exports = [
+  {
+    fileName: 'popcorn.html',
+    thumbnail: './img/popcorn-thumbnail.jpg',
+    images: [
+      './img/popcorn-1.jpg',
+      './img/popcorn-2.jpg',
+    ],
+    text: [
+      'ポップコーンの説明ポップコーンの説明ポップコーンの説明ポップコーンの説明ポップコーンの説明ポップコーンの説明ポップコーンの説明ポップコーンの説明ポップコーンの説明ポップコーンの説明ポップコーンの説明ポップコーンの説明ポップコーンの説明',
+    ],
+  },
+  {
+    fileName: 'SLG.html',
+    thumbnail: './img/SLG-thumbnail.jpg',
+    images: [
+      './img/SLG-1.png',
+      './img/SLG-2.png',
+      './img/SLG-3.png',
+      './img/SLG-4.png',
+    ],
+    text: [
+      'ターン制のシュミレーションゲームです． 比較的難易度は高めになっています．',
+      '情報科学科二年の情報科学演習で作成しました．Javaで実装されています．',
+      'システム制作で力尽きたため，ステージ自体はシンプルでバランス調整もままならない状況になっています．一応クリアは可能です．',
+    ],
+  },
+];
--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,6 @@ module.exports = {
       },
     ],
   },
-  devtool: 'source-map',
   plugins: [
     new webpack.optimize.UglifyJsPlugin({
       sourceMap: true,
