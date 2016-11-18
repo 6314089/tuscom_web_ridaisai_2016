230525ab0adaa85e52af2028d11243160d67517f
satorf <satorf.t@ed.tus.ac.jp>
2016-11-18 01:40:53 +0000

    html整形をするように

--- compilePug.js
+++ compilePug.js
@@ -1,19 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 const pug = require('pug');
+const html = require('html');
 const data = require('./src/data');
 
 const dist = path.join(__dirname, '/dist');
-const option = {
-  pretty: true,
-};
 
-const top = pug.compileFile('./src/pug/top.pug', option);
-const single = pug.compileFile('./src/pug/single.pug', option);
+const top = pug.compileFile('./src/pug/top.pug');
+const single = pug.compileFile('./src/pug/single.pug');
 
 fs.writeFile(
   path.join(dist, 'top.html'),
-  top({}),
+  html.prettyPrint(top({})),
   (err) => {
     if (err) throw err;
     console.log('top.html 完了');
@@ -22,7 +20,7 @@ fs.writeFile(
 data.forEach((obj) => {
   fs.writeFile(
     path.join(dist, obj.fileName),
-    single(obj),
+    html.prettyPrint(single(obj)),
     (err) => {
       if (err) throw err;
       console.log(`${obj.fileName} 完了`);
