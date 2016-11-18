c8c99686fc1576a637b8072a2dc5a9361268b0d2
6314089 <6314089@ed.tus.ac.jp>
2016-11-18 06:07:24 +0000

    pugのcompileスクリプトを書いた

--- compilePug.js
+++ compilePug.js
@@ -2,27 +2,50 @@ const fs = require('fs');
 const path = require('path');
 const pug = require('pug');
 const html = require('html');
-const data = require('./src/data');
 
+const htmlOpts = { unformatted: [] };
 const dist = path.join(__dirname, '/dist');
+
+const data = require('./src/data');
 
 const top = pug.compileFile('./src/pug/top.pug');
 const single = pug.compileFile('./src/pug/single.pug');
 
-fs.writeFile(
-  path.join(dist, 'top.html'),
-  html.prettyPrint(top({}), { unformatted: [] }),
-  (err) => {
-    if (err) throw err;
-    console.log('top.html 完了');
-  });
+const compileTop = () => {
+  const out = path.join(dist, 'top.html');
+  const d = {
+    title: '理大祭2016 - TUSCOM',
+    data,
+  };
 
-data.forEach((obj) => {
   fs.writeFile(
-    path.join(dist, obj.fileName),
-    html.prettyPrint(single(obj), { unformatted: [] }),
+    out,
+    html.prettyPrint(top(d), htmlOpts),
     (err) => {
       if (err) throw err;
-      console.log(`${obj.fileName} 完了`);
+      console.log('  compiled top.html');
     });
-});
+};
+
+const compileSingles = () => {
+  const length = data.length;
+  data.forEach((obj, index, arr) => {
+    const out = path.join(dist, obj.fileName);
+    const d = Object.assign({
+      prev: arr[((length + index) - 1) % length].fileName,
+      next: arr[((length + index) + 1) % length].fileName,
+    }, obj);
+    d.title += ' : 理大祭2016 - TUSCOM';
+
+    fs.writeFile(
+      out,
+      html.prettyPrint(single(d), htmlOpts),
+      (err) => {
+        if (err) throw err;
+        console.log(`  compiled ${obj.fileName}`);
+      });
+  });
+};
+
+compileTop();
+compileSingles();
