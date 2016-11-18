7d896d5477cff43a401f7beab495a6e6c4959487
satorf <satorf.t@ed.tus.ac.jp>
2016-11-18 19:36:19 +0000

    画像ファイルのディレクトリを切った

--- buildImage.js
+++ buildImage.js
@@ -5,37 +5,48 @@ const mkdirp = require('mkdirp-promise');
 const imagemin = require('imagemin');
 const svg2png = require('svg2png');
 
+
 const src = path.join(__dirname, './src/img/');
 const dist = path.join(__dirname, './dist/img/');
 const pattern = ext => `${src}**/*.${ext}`;
 
-const other = () =>
-  imagemin([pattern('png'), pattern('jpg')], dist)
-  .then(files => files.map(
-    file => console.log(`complete ${file.path}`)
-  ));
 
-const svg = () =>
-  glob(pattern('svg'))
-  .then(files => files.map(
-    file => [file, path.relative(src, file).replace(/.svg$/, '.png')]
-  ))
-  .then(files => files.map(
-    ([file, temp]) => [file, path.join(dist, temp)]
-  ))
-  .then(files => files.map(
-    ([file, out]) => mkdirp(path.dirname(out))
-      .then(() => fs.readFile(file))
-      .then(svg2png)
-      .then(imagemin.buffer)
-      .then(buffer => fs.writeFile(out, buffer))
-      .then(() => console.log(`complete ${out}`))
-      .catch(err => console.error(err))
-  ))
+const eachFiles = (pat, func) =>
+  glob(pat)
+  .then(files => files.map(file => ({
+    file,
+    temp: path.relative(src, file).replace(/.svg$/, '.png'),
+  })))
+  .then(files => files.map(({ file, temp }) => ({
+    file,
+    out: path.join(dist, temp)
+  })))
+  .then(files => files.map(func))
   .then(pendings => Promise.all(pendings))
 
+const other = () => eachFiles(
+  pattern('{png,jpg}'),
+  ({ file, out }) => mkdirp(path.dirname(out))
+    .then(() => fs.readFile(file))
+    .then(imagemin.buffer)
+    .then(buffer => fs.writeFile(out, buffer))
+    .then(() => console.log(`complete ${out}`))
+    .catch(err => console.error(err))
+);
 
-Promise.resolve(console.log('build images'))
+const svg = () => eachFiles(
+  pattern('svg'),
+  ({ file, out }) => mkdirp(path.dirname(out))
+    .then(() => fs.readFile(file))
+    .then(svg2png)
+    .then(imagemin.buffer)
+    .then(buffer => fs.writeFile(out, buffer))
+    .then(() => console.log(`complete ${out}`))
+    .catch(err => console.error(err))
+);
+
+
+Promise.resolve(console.log('build images start\n'))
 .then(() => Promise.all([other(), svg()]))
-.then(() => console.log('\ncomplete!'))
+.then(() => console.log('\nbuild images complete!'))
 .catch(err => console.error(err));
