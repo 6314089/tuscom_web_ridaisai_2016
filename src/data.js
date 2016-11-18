b8811cfceead987f1850be2c559b5b60ffe68b7e
satorf <satorf.t@ed.tus.ac.jp>
2016-11-18 06:07:59 +0000

    データを用意した

--- src/data.js
+++ src/data.js
@@ -6,23 +6,67 @@ module.exports = [
       './img/popcorn-1.jpg',
       './img/popcorn-2.jpg',
     ],
+    title: 'ポップコーン販売（外ブース）',
     text: [
       'ポップコーンの説明ポップコーンの説明ポップコーンの説明ポップコーンの説明ポップコーンの説明ポップコーンの説明ポップコーンの説明ポップコーンの説明ポップコーンの説明ポップコーンの説明ポップコーンの説明ポップコーンの説明ポップコーンの説明',
     ],
   },
   {
     fileName: 'SLG.html',
-    thumbnail: './img/SLG-thumbnail.jpg',
+    thumbnail: './img/SLG-thumbnail.png',
     images: [
       './img/SLG-1.png',
       './img/SLG-2.png',
       './img/SLG-3.png',
       './img/SLG-4.png',
     ],
+    title: 'SLG',
     text: [
-      'ターン制のシュミレーションゲームです． 比較的難易度は高めになっています．',
+      'ターン制のシミュレーションゲームです． 比較的難易度は高めになっています．',
       '情報科学科二年の情報科学演習で作成しました．Javaで実装されています．',
       'システム制作で力尽きたため，ステージ自体はシンプルでバランス調整もままならない状況になっています．一応クリアは可能です．',
+    ],
+  },
+  {
+    fileName: 'dummy1.html',
+    thumbnail: './img/dummy1-thumbnail.png',
+    images: [
+      './img/dummy1-1.png',
+      './img/dummy1-2.png',
+    ],
+    title: 'dummy1',
+    text: [
+      'dummy1dummy1dummy1dummy1dummy1dummy1dummy1',
+      'dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1',
+      'dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1dummy1',
+    ],
+  },
+  {
+    fileName: 'dummy2.html',
+    thumbnail: './img/dummy2-thumbnail.png',
+    images: [
+      './img/dummy2-1.png',
+      './img/dummy2-2.png',
+    ],
+    title: 'dummy2',
+    text: [
+      'dummy2dummy2dummy2dummy2dummy2dummy2dummy2',
+      'dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2',
+      'dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2dummy2',
+    ],
+  },
+  {
+    fileName: 'dummy3.html',
+    thumbnail: './img/dummy3-thumbnail.png',
+    images: [
+      './img/dummy3-1.png',
+      './img/dummy3-2.png',
+    ],
+    title: 'dummy3',
+    text: [
+      'dummy3dummy3dummy3dummy3dummy3dummy3dummy3',
+      'dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3',
+      'dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3dummy3',
     ],
   },
 ];
