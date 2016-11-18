e78c5d1993af9356f1d37a37c99d949b383c30cc
satorf <satorf.t@ed.tus.ac.jp>
2016-11-18 16:14:06 +0000

    ディレクトリを作るようにした

--- svgToPng.js
+++ svgToPng.js
@@ -1,19 +1,21 @@
 const path = require('path');
-const fsp = require('fs-promise');
+const mkdirp = require('mkdirp-promise');
+const fs = require('fs-promise');
 const glob = require('glob-promise');
 const svg2png = require('svg2png');
 
 const pattern = './src/img/*.svg';
 const dist = path.join(__dirname, './dist/img/');
 
-glob(pattern, { nodir: true })
+mkdirp(dist)
+.then(() => glob(pattern, { nodir: true }))
 .then(files => Promise.all(
   files
   .map(file => [file, file.split('/').pop().replace(/.svg$/, '.png')])
   .map(([file, fileName]) =>
-    fsp.readFile(file)
+    fs.readFile(file)
     .then(svg2png)
-    .then(buffer => fsp.writeFile(path.join(dist, fileName), buffer))
+    .then(buffer => fs.writeFile(path.join(dist, fileName), buffer))
     .then(() => console.log(`svg to png  ${fileName}`))
     .catch(err => console.error(err)))))
 .then(() => console.log('\ncomplete!'))
